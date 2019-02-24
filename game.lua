fa33ba1ee6d7692f8dc5ec63dcc894ef04a9ab90
bridgs <bridgs.dev@gmail.com>
2019-02-23 21:51:28 -0500

    Adding bullets

--- game.lua
+++ game.lua
@@ -4,15 +4,17 @@ local Moat = require('lib/moat')
 -- Render constants
 local GAME_WIDTH = 200
 local GAME_HEIGHT = 200
-local RENDER_SCALE = 2
+local RENDER_SCALE = 3
 
 -- Define a unique ID for each type of entity that can exist
 local ENTITY_TYPES = {
-  Player = 0
+  Player = 0,
+  Bullet = 1
 }
 
 -- Define some constants that configure the way Moat shares client data
 local MOAT_CONFIG = {
+  TickInterval = 1.0 / 60.0,
   WorldSize = math.max(GAME_WIDTH, GAME_HEIGHT),
   ClientVisibility = math.max(GAME_WIDTH, GAME_HEIGHT)
 }
@@ -31,26 +33,69 @@ function Game:clientLoad()
 end
 
 -- Updates the game state
-function Game:worldUpdate(state)
-end
 function Game:serverUpdate(state)
 end
 function Game:clientUpdate(state)
   if self:clientIsSpawned() then
-    Game:setPlayerInput({
+    local player = self:getPlayerState()
+    local aimX = love.mouse.getX() / RENDER_SCALE
+    local aimY = love.mouse.getY() / RENDER_SCALE
+    local aimAngle = math.atan2(aimY - player.y - player.w / 2, aimX - player.x - player.h / 2)
+    self:setPlayerInput({
       up = love.keyboard.isDown('up') or love.keyboard.isDown('w'),
       left = love.keyboard.isDown('left') or love.keyboard.isDown('a'),
       down = love.keyboard.isDown('down') or love.keyboard.isDown('s'),
-      right = love.keyboard.isDown('right') or love.keyboard.isDown('d')
+      right = love.keyboard.isDown('right') or love.keyboard.isDown('d'),
+      shoot = love.mouse.isDown(1),
+      aimAngle = aimAngle
     })
   end
 end
 function Game:playerUpdate(player, input)
-  if input then 
-    player.x = clamp(0, player.x + (input.right and 1 or 0) - (input.left and 1 or 0), GAME_WIDTH - player.width)
-    player.y = clamp(0, player.y + (input.down and 1 or 0) - (input.up and 1 or 0), GAME_HEIGHT - player.height)
-    Game:moveEntity(player)
+  if input then
+    player.vx = 32 * ((input.right and 1 or 0) - (input.left and 1 or 0))
+    player.vy = 32 * ((input.down and 1 or 0) - (input.up and 1 or 0))
+    player.x = clamp(0, player.x + player.vx * MOAT_CONFIG.TickInterval, GAME_WIDTH - player.w)
+    player.y = clamp(0, player.y + player.vy * MOAT_CONFIG.TickInterval, GAME_HEIGHT - player.h)
+    self:moveEntity(player)
+    player.shootCooldown = math.max(0.00, player.shootCooldown - MOAT_CONFIG.TickInterval)
+    if input.shoot and input.aimAngle and player.shootCooldown <= 0.00 then
+      player.shootCooldown = 1.00
+      self:spawn(ENTITY_TYPES.Bullet, player.x + player.w / 2 - 3, player.y + player.h / 2 - 3, 6, 6, {
+        vx = 64 * math.cos(input.aimAngle),
+        vy = 64 * math.sin(input.aimAngle),
+        ownerClientId = player.clientId
+      })
+    end
   end
+end
+function Game:worldUpdate(state)
+  -- Move bullets and check for hits
+  self:eachEntityOfType(ENTITY_TYPES.Bullet, function(bullet)
+    bullet.x = bullet.x + bullet.vx * MOAT_CONFIG.TickInterval
+    bullet.y = bullet.y + bullet.vy * MOAT_CONFIG.TickInterval
+    self:moveEntity(bullet)
+
+    -- Check for bullet hits
+    self:eachOverlapping(bullet, function(entity)
+      -- Bullets push players back a bit
+      if entity.type == ENTITY_TYPES.Player and entity.clientId ~= bullet.ownerClientId then
+        entity.x = entity.x + 6 * bullet.vx * MOAT_CONFIG.TickInterval
+        entity.y = entity.y + 6 * bullet.vy * MOAT_CONFIG.TickInterval
+        self:moveEntity(entity)
+        self:despawn(bullet)
+      -- Bullets cancel each other out
+      elseif entity.type == ENTITY_TYPES.Bullet and entity.ownerClientId ~= bullet.ownerClientId then
+        self:despawn(entity)
+        self:despawn(bullet)
+      end
+    end)
+
+    -- Despawn bullets that go off screen
+    if bullet.x > GAME_WIDTH or bullet.y > GAME_HEIGHT or bullet.x < -bullet.w or bullet.y < -bullet.h then
+      self:despawn(bullet)
+    end
+  end)
 end
 
 -- Draws the game
@@ -62,10 +107,16 @@ function Game:clientDraw()
   love.graphics.setColor(50 / 255, 0 / 255, 100 / 255)
   love.graphics.rectangle('fill', 0, 0, GAME_WIDTH, GAME_HEIGHT)
 
+  -- Draw all the bullets
+  self:eachEntityOfType(ENTITY_TYPES.Bullet, function(bullet)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.rectangle('fill', bullet.x, bullet.y, bullet.w, bullet.h)
+  end)
+
   -- Draw all players
   self:eachEntityOfType(ENTITY_TYPES.Player, function(player)
     love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
+    love.graphics.rectangle('fill', player.x, player.y, player.w, player.h)
   end)
 end
 
@@ -83,11 +134,15 @@ function Game:serverReceive(clientId, msg)
     self:spawnPlayer(clientId)
   end
 end
+-- The player spawns at a random location
 function Game:serverResetPlayer(player)
   player.x = math.random(20, GAME_WIDTH - 20)
   player.y = math.random(20, GAME_WIDTH - 20)
-  player.width = 10
-  player.height = 10
+  player.w = 16
+  player.h = 16
+  player.vx = 0
+  player.vy = 0
+  player.shootCooldown = 0.00
 end
 
 -- Keeps a number between the given minimum and maximum values
